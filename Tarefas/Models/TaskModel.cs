2cb5a2118cf7c02694643183cf6e2af10b30a400
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-29 15:33:10 -0500

    criando model de tarefa

---
+++ Tarefas/Models/TaskModel.cs
@@ -0,0 +1,9 @@
+namespace Tarefas.Models;
+
+public class TaskModel
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public int status { get; set; }
+}
