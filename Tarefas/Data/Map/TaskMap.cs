5333497768d031da13a7daf28c57835ca197c640
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-29 17:34:30 -0500

    criando map de tarefas

---
+++ Tarefas/Data/Map/TaskMap.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tarefas.Models;
+
+namespace Tarefas.Data.Map;
+
+public class TaskMap : IEntityTypeConfiguration<TaskModel>
+{
+    public void Configure(EntityTypeBuilder<TaskModel> builder)
+    {
+        builder.HasKey(x => x.id);
+        builder.Property(x => x.name).IsRequired().HasMaxLength(255);
+        builder.Property(x => x.description).HasMaxLength(1000);
+        builder.Property(x => x.status).IsRequired();
+    }
+}
