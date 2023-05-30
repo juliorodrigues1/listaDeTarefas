d8a3ba976fd0d20182ab3b90e9c13fe7953ed332
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-30 08:34:24 -0500

    removendo arquivos

---
+++ Tarefas/Migrations/20230530132608_vinculoTarefaUsuario.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarefas.Migrations
+{
+    public partial class vinculoTarefaUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "Tasks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_userId",
+                table: "Tasks",
+                column: "userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_userId",
+                table: "Tasks",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_userId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_userId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Tasks");
+        }
+    }
+}
